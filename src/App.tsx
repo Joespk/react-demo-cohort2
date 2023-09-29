2c15a883c26470e0c2249e709d111ff69189a57e
Joespk <supakritjoe@gmail.com>
2023-09-29 15:41:56 +0700

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Greeting from './components/Greeting'
+import Navbarheading from './components/Navbarheading'
 
 function App() {
   return (
     <div className="App">
+      <Navbarheading />
       <Greeting />
     </div>
   )
