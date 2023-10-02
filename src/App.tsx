025cee8a762a1c61425371efd30ea9fd515c04a5
Joespk <supakritjoe@gmail.com>
2023-10-02 14:57:31 +0700

    demo handle

--- src/App.tsx
+++ src/App.tsx
@@ -23,16 +23,22 @@ const posts: PostDTO[] = [
     title: 'Basic HTML',
     body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
   },
+  {
+    id: 4,
+    userId: 4,
+    title: 'CSS',
+    body: 'To understand how we provide styling for web pages\n To understand how CSS syntax\nDemonstrate how to apply CSS into HTML elements',
+  },
 ]
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Greeting name="Bun" greetingMsg="Hello!" isLoggedIn={true} />
+      <Greeting name="Joe" greetingMsg="Hello!" isLoggedIn={true} />
       <div className="feed-container">
-        {posts.map((post) => {
-          return <Post key={post.id} post={post} />
+        {posts.map((postValue) => {
+          return <Post key={postValue.id} post={postValue} />
         })}
       </div>
     </div>
